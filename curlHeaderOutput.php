d9f5023732a324654274c923d99d299380f1caf9
Michael Alaimo <malaimo923@gmail.com>
2015-01-09 16:57:58 -0500

    Updated to use the common php libraries Curl object

--- curlHeaderOutput.php
+++ curlHeaderOutput.php
@@ -1,43 +1,72 @@
 <?php
 
+$common_php_dir = '../php_common';
+$autoload_file = $common_php_dir.'/autoload.php';
+
+require($autoload_file);
+
+use \common\curl\Main as curl;
+
+
 $ua = 'Mozilla/5.0 (Android; Mobile; rv:30.0) Gecko/30.0 Firefox/30.0';
 $fileHeader = fopen(__DIR__.'/http_headers.txt', 'w+');
 $cookieFile = __DIR__.'/http_cookies.txt';
 
 if (isset($argv[1]) && ($url = filter_var($argv[1], FILTER_VALIDATE_URL))) {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-	curl_setopt($ch, CURLOPT_WRITEHEADER, $fileHeader);
-	curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieFile);
-	curl_setopt($ch, CURLOPT_USERAGENT, $ua);
+	$curl = new curl(FALSE);
 	
-	$response = curl_exec($ch);
+	$curl->create();
 	
-	$effectiveUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-	$redirectCount = curl_getinfo($ch, CURLINFO_REDIRECT_COUNT);
-	$redirectTime = curl_getinfo($ch, CURLINFO_REDIRECT_TIME);
+	$curl->addOption(CURLOPT_URL, $url);
+	$curl->addOption(CURLOPT_RETURNTRANSFER, TRUE);
+	$curl->addOption(CURLOPT_FOLLOWLOCATION, TRUE);
+	$curl->addOption(CURLOPT_WRITEHEADER, $fileHeader);
+	$curl->addOption(CURLOPT_COOKIEJAR, $cookieFile);
+	$curl->addOption(CURLOPT_USERAGENT, $ua);
+	
+	$curl->run();
+	$response = $curl->getOutput();
+	
+	$info = array_filter($curl->info(), function($v) { 
+		if (in_array($v[0], array(CURLINFO_EFFECTIVE_URL, CURLINFO_REDIRECT_COUNT, CURLINFO_REDIRECT_TIME), TRUE)) { 
+			$result =  TRUE; } else { $result = FALSE; } return $result;
+	});
+	$position = array_colum($info, 0);
+	
+	$effectiveUrl = (($i = array_search(CURLINFO_EFFECTIVE_URL, $position, TRUE)) && isset($info[$i]) ? $info[$i] : '');
+	$redirectCount = (($i = array_search(CURLINFO_REDIRECT_COUNT, $position, TRUE)) && isset($info[$i]) ? $info[$i] : '');
+	$redirectTime = (($i = array_search(CURLINFO_REDIRECT_TIME, $position, TRUE)) && isset($info[$i]) ? $info[$i] : '');
+	
 	
 	rewind($fileHeader);
 	$locations = [];
 	while(($row = fgets($fileHeader)) !== FALSE) {
-		if (stripos($row, 'location:') === 0) {
-			$locations[] = str_ireplace('location:', '', trim($row));
+		if (stripos($row, 'location:') !== FALSE) {
+			$locations[] = trim(str_ireplace('location:', '', $row));
 		}
 	}
-	curl_close($ch);
-	unset($ch, $row, $location);
+	unset($row);
 	
 	echo "Initial URL: {$url}\n";
-	echo "Effective URL: {$effectiveUrl}\n";
-	echo "Redirect Count: {$redirectCount}\n";
-	echo "Redirect Time: {$redirectTime}\n";
+	foreach ($info as $i) {
+		switch ($i[0]) {
+			case CURLINFO_EFFECTIVE_URL:
+				$text = 'Effective URL:';
+				break;
+			case CURLINFO_REDIRECT_COUNT:
+				$text = 'Redirect Count:';
+				break;
+			case CURLINFO_REDIRECT_TIME;
+				$text = 'Redirect Time:';
+				break;
+		}
+		echo $text.' '.$i[1]."\n";
+	}
 	echo "Locations:\n\t".implode("\n\t", $locations)."\n";
 	echo "HTTP Cookie Data:\n\t".implode("\n\t", array_map('trim', file($cookieFile)))."\n";
-
+	
 	unset($locations, $effectiveUrl, $rediriectCount, $redirectTime);
- 
+ 	
 	$return = 0;
 } else {
 	echo "Please supply a valid URL";
