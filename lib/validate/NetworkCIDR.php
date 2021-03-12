5022f0968f319de7bfbd1fd8552efe476fe6c676
Michael Alaimo <michaelalaimo@gmail.com>
2021-03-12 13:50:05 -0500

    Correct bug with the way filer_var options were used

--- lib/validate/NetworkCIDR.php
+++ lib/validate/NetworkCIDR.php
@@ -5,11 +5,27 @@ namespace lib\validate;
 use \common\obj as obj;
 
 class NetworkCIDR extends obj\Config {
-	protected $ip;
-        protected $cidr;
 
-	protected $config = array(
-			'cidr' => array(FILTER_VALIDATE_INT, array('min_range' => 0, 'max_range' => 32)),
-                        'ip' => array(FILTER_VALIDATE_IP, array(FILTER_FLAG_IPV4))
-	);
+    protected $ip;
+    protected $cidr;
+    protected $config = [
+        'cidr' => [
+            FILTER_VALIDATE_INT,
+            [
+                'options' => [
+                    'min_range' => 0,
+                    'max_range' => 32
+                ]
+            ]
+        ],
+        'ip' => [
+            FILTER_VALIDATE_IP,
+            [
+                'flags' => [
+                    FILTER_FLAG_IPV4
+                ]
+            ]
+        ]
+    ];
+
 }
