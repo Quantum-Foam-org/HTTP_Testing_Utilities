04ec89399f8d2e89dc360615930348a12b724af5
Michael Alaimo <michaelalaimo@gmail.com>
2021-03-12 14:45:12 -0500

    Move tests into the classes folder

--- tests/autoload.php
+++ tests/autoload.php
@@ -7,7 +7,7 @@ spl_autoload_register(function ($class) {
     if (strpos($class, $namespace) === 0) {
         $class = substr($class, strlen($namespace)-1);
         
-        $classFile = realpath(__DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+        $classFile = realpath(__DIR__ . '/classes/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
         
         if (file_exists($classFile)) {
             require ($classFile);
