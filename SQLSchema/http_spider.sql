48fd9748f45578d067777a3177fe0c0c5ab066f9
maaj <malaimo923@gmail.com>
2021-05-19 14:22:35 -0400

    MySQL 8 backup

--- SQLSchema/http_spider.sql
+++ SQLSchema/http_spider.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
 --
 -- Host: localhost    Database: http_spider
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `spidered_site`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `spidered_site` (
   `id` int NOT NULL AUTO_INCREMENT,
   `url` varchar(1500) NOT NULL,
@@ -30,10 +30,19 @@ CREATE TABLE `spidered_site` (
   `redirect_count` smallint unsigned NOT NULL,
   `response_length` int NOT NULL,
   `response_body` mediumtext,
-  `content_type` varchar(400) NOT NULL
+  `content_type` varchar(400) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spidered_site`
+--
+
+LOCK TABLES `spidered_site` WRITE;
+/*!40000 ALTER TABLE `spidered_site` DISABLE KEYS */;
+/*!40000 ALTER TABLE `spidered_site` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -44,4 +53,4 @@ CREATE TABLE `spidered_site` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-16 11:05:27
+-- Dump completed on 2021-05-19 18:21:34
